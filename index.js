8ac44a73b6116e5671dc6153f7905224e80e04a8
Rod Gaitan <rodgaitan01@gmail.com>
2026-01-16 16:18:58 -0500

    completed the last task 'Test Your Application'

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ let inputEl = document.getElementsByClassName("input")
 let taskListElTwo = document.getElementById("searchdisplay")
 
 
+// LS-1: Load tasks from local storage into array, if available
+let savedTasks = localStorage.getItem("tasks") // 1)gets 'tasks' items from local storage
+if (savedTasks) {
+    tasks = JSON.parse(savedTasks); // 2) de-stringifies extracted data from Local Storage and stores in 'tasks' array
+    renderTask(); // 3) calls function 'renderTask()' to render 'tasks' items stored in array
+}
+
+
+
+///////////////////////////////////////// local storage ////////////////////////
+/*
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+   
+    // To verify that it works:
+    //console.log( localStorage.getItem("myLeads") ) // 1) one way to verify and got confirmation that the array items are being extracted
+    //let leadsFromLocalStorage = localStorage.getItem("myLeads") //2) another way is to store it in a variable and...
+    //console.log(leadsFromLocalStorage) //3) the verify and get confirmation that the array items are being extracted by console.logging out the variable
+   
+    localStorage.clear()
+
+    let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) //4) a 3rd way to extract the data from local storage BUT HERE we converting back to non-string data.
+    console.log(leadsFromLocalStorage) //5) the verify and get confirmation that the array items are being extracted by console.logging out the variable.
+})
+    */
+
+//////////////////////////////////////////////////////////////////////////////////
+
 btnEl.addEventListener("click", addNewtask)
 
 function addNewtask(category, task, deadline, status) {
@@ -28,6 +59,22 @@ function addNewtask(category, task, deadline, status) {
 
     tasks.push(newTask)
     console.log(newTask)
+
+
+
+    
+    // LS-1A: stringifies and pushes items from array to Local Storage
+    localStorage.setItem("tasks", JSON.stringify(tasks)) // When "Add task" button is clicked, this saves the new task to Local Storage
+
+    /*
+    localStorage.setItem("tasks", JSON.stringify(tasks) )
+    console.log(localStorage.getItem("tasks"))
+    //localStorage.clear()  
+    let tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"))
+     console.log(tasksFromLocalStorage) 
+    */
+
+     
     renderTask()
     return newTask
 
@@ -49,7 +96,8 @@ function checkOverDueTasks() {
         }
 
     }
-
+    // LS-1B: stringifies and pushes items from array to Local Storage
+    localStorage.setItem("tasks", JSON.stringify(tasks)); //when a task's status in 'tasks' becomes overdue, this saves the change in status Local Storage
 }
 
 btnElTwo.addEventListener("click", searchBy)
@@ -116,7 +164,7 @@ function searchBy() {
 function renderTask() {
 
     checkOverDueTasks()
-    //inputEl.value = ""  go back to this as it is not clearing my fields once I click the button!!!!!!
+    //inputEl.value = "" //go back to this as it is not clearing my fields once I click the button!!!!!!
     taskListEl.innerHTML = "" //clears the list so it can be re-rendered without duplicating items
 
 
@@ -153,9 +201,11 @@ function renderTask() {
         });
 
 
-        function updateStatus(index, newStatus) {
-            tasks[index].status = newStatus;
+        function updateStatus(i, newStatus) {
+            tasks[i].status = newStatus;
             console.log(tasks)
+            // Save updated tasks to local storage
+            localStorage.setItem("tasks", JSON.stringify(tasks));
             renderTask();
         }
 
